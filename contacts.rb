99c69d514c471c34dc750edf2118a7f4b48ec4c4
ihollander <ianholla@gmail.com>
2021-07-09 17:10:05 -0400

    commit tree 5b3c40904462d2198fb36f0ff2c4bce4ddb97463

---
+++ contacts.rb
@@ -0,0 +1,34 @@
+require 'pry'
+
+# this method returns a hash, which you can pass to the remove_strawberry method:
+# remove_strawberry(contacts)
+def contacts
+  {
+    "Jon Snow" => {
+      name: "Jon",
+      email: "jon_snow@thewall.we",
+      favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+    },
+    "Freddy Mercury" => {
+      name: "Freddy",
+      email: "freddy@mercury.com",
+      favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+    }
+  }
+end
+
+def remove_strawberry(contacts)
+  # your code here!
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if { |ice_cream| ice_cream == "strawberry" }
+        end
+      end
+    end
+  end
+end
+
+# print the output to the terminal for inspection
+pp remove_strawberry(contacts)
